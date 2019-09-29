dc12a5b674d2e27bf58c039f8c64acb533f2c360
mazunki <rolferen@gmail.com>
2019-09-29 20:56:48 +0200

    comments

--- IN1000/oblig5/fibonaccifile.py
+++ IN1000/oblig5/fibonaccifile.py
@@ -4,13 +4,21 @@
 """
 	This program is made as a solution to activity 5 of the 5th assignment of
 	IN1000 at UiO, fall 2019.
+
+	The idea is to use a file to store Fibonacci numbers to, 
+	so that these can be used to calculate more Fibonacci numbers when desired.
+
+	Use `sed -n "N p" fibonacci.txt` to print the desired Fibonacci number, where
+	N is the Nth Fibonacci number. Outputs nothing if the number is not in the list yet.
+
+	Use `wc -l fibonacci.txt` to print the amount of Fibonacci numbers found.
 """
 __author__ = "Rolf Vidar Hoksaas"
 __email__ = "rolferen@gmail.com"
-__date__ = "19th September 2019"
+__date__ = "29th September 2019"
 
 import time
-STACK = 1e3+2
+STACK = 1e3+2  # be careful not to go crazy here. 1e4 is already slow for me.
 FIB_FILE = "fibonacci.txt"
 fib_list = list()
 
